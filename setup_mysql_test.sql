475b0f1935938782a000a9c4030e28ded965f07d
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-22 11:18:27 +0300

    added tasks

---
+++ setup_mysql_test.sql
@@ -0,0 +1,18 @@
+-- Creates a MySQL server with:
+--   Database hbnb_test_db.
+--   User hbnb_test with password hbnb_test_pwd in localhost.
+
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_test_db`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
