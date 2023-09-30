62b0946ce2e804550b67a6d36c4ddeec7bf14c0d
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-09-30 12:08:32 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""Defines the User class."""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents a user for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    reviews = relationship("Review", backref="user", cascade="delete")
+    places = relationship("Place", backref="user", cascade="delete")
+   
